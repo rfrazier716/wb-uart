3d3df7092ae2a5c1d81c2958b4e6ac05f279046e
Ryan Frazier <rfrazier716@gmail.com>
2020-11-18 08:08:21 -0500

    cleaned up cmake to make adding tests easier

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,6 +1,47 @@
+# Define All unittests here, for larger projects, tests can be split into separate CMAKE files
+
 set(TEST_DIR "${CMAKE_CURRENT_LIST_DIR}/unittests")
 message("Building UnitTests in ${TEST_DIR}")
-#Individual Unittests included below
-#-------------------------------------------------
-include("${TEST_DIR}/test_UARTTx.cmake")
-include("${TEST_DIR}/test_FIFO.cmake")
+
+######################################################
+## Testing that the UART Transmit Block is Functional
+######################################################
+
+# Create new Target
+add_executable(test_UARTTx
+    "bench/cpp/src/verilatorCatchMain.cpp"
+    "bench/cpp/src/testUARTTx.cpp"
+    ${VERILATED}
+    ${VERILATED_TRACE}
+)
+
+# Add a dependency to the verilator generated libraries
+add_dependencies(test_UARTTx vl_libs)
+
+# Link to the Verilator Generated static library
+target_link_libraries(test_UARTTx ${VL_UART_TX}) 
+
+# Register a test to this target
+add_test(UARTTX_FUNC test_UARTTx)
+
+
+######################################################
+## Testing that the FIFO Block is Functional
+######################################################
+
+# Create new Target
+add_executable(test_FIFO
+    "bench/cpp/src/verilatorCatchMain.cpp"
+    "bench/cpp/src/testFifo.cpp"
+    ${VERILATED}
+    ${VERILATED_TRACE}
+)
+
+# Add a dependency to the verilator generated libraries
+add_dependencies(test_FIFO vl_libs)
+
+# Link to the Verilator Generated static library
+target_link_libraries(test_FIFO ${VL_FIFO}) 
+
+# Register a test to this target
+add_test(FIFO_FUNC test_FIFO)
--- cmake/verilator_build.cmake
+++ cmake/verilator_build.cmake
@@ -1,12 +1,24 @@
+# This file regenerates the Verilator library files by creating custom commands that generated the outputed files
+# and a custom target that depends on those output files
+# For any source file that depends on these libraries you must use put ADD_DEPENDENCIES(TARGET vl_libs) in order to ensure they're built before the target is
+
 if(${REBUILD_VERILATOR})
-    add_custom_target(UART_TX_vl ALL
+
+    add_custom_command(OUTPUT ${VL_UART_TX}
         COMMAND sh verilator_uart_tx.sh
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/verilator/"
-        )
+    )
 
-    add_custom_target(FIFO_vl ALL
+    add_custom_command(OUTPUT ${VL_FIFO}
         COMMAND sh verilator_fifo.sh
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/verilator/"
-        )
+    )
+    
+endif()
 
-ENDIF()
+add_custom_target(vl_libs ALL
+    COMMAND echo -e "Checking Verilator Libraries Exist"
+    DEPENDS
+        ${VL_UART_TX}
+        ${VL_FIFO}
+)
