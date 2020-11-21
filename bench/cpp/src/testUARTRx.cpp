214dacc0c4d282d0d5e4607d2a4eb5a7cc283f64
Ryan Frazier <rfrazier716@gmail.com>
2020-11-20 20:54:23 -0500

    finished unittests for RX core

--- bench/cpp/src/testUARTRx.cpp
+++ bench/cpp/src/testUARTRx.cpp
@@ -18,6 +18,7 @@
 
 // Additional parameter definition, this is also set in the verilator .sh file
 #define TICKS_PER_CYCLE 8
+#define ST_IDLE 10
 //State machine registers and paramters
 
 template<class T>
@@ -26,30 +27,96 @@ void logSignal(T signal)
     printf("0x%02x\n",signal);
 }
 
-//Cases to Test
-//Program counter reset
-TEST_CASE("Single Byte Transmission","[uart-tx]"){
+
+void receiveByte(SyncTB<MODTYPE>* testbench, int dataByte){
+    auto dataPacket = (dataByte | 0x100)<<1;
+    for(int k=0;k<10;k++)
+    {
+        testbench->dut->i_rx_w=((dataPacket>>k) & 0x01); // update the bit on the data register
+        for(int j=0;j<TICKS_PER_CYCLE;j++)
+        {
+            testbench->tick();
+        }
+    }
+}
+
+TEST_CASE("Single Byte Transmission","[uart-rx]"){
     /*
-    This is really just here to make a gtkwave plot in the build directory that can be referenced for debug
+    Makes a GTKWave plot of a uart Receive frame and verifies the correct value was latched
     */
 
     auto* tb = new SyncTB<MODTYPE>(50000000, false); // make a new module test bench
     tb->addVCDTrace("UART_SINGLERX.vcd");
     tb->tick(); // Tick the clock once
-    tb->dut->i_rx_w = 0; //put data on the bus
-    auto data_message = (0xA5+(1<<8))<<1; // assign a message and put the stop bit at the end, and start bit at beginning
-    // iterate over the incoming message
-    for(int k=0;k<10;k++)
-    {
-        tb->dut->i_rx_w=((data_message>>k) & 0x01); // update the bit on the data register
-        for(int j=0;j<TICKS_PER_CYCLE;j++)
-        {
-            tb->tick();
-        }
-    }
-    // add extra ticks
-    for(int j=0;j<TICKS_PER_CYCLE;j++)
+    
+    //Transmit a byte and make sure the right value is received
+    auto transmissionByte = 0xA5;
+    receiveByte(tb, transmissionByte);
+    REQUIRE(tb->dut->o_data_w==transmissionByte);
+}
+
+TEST_CASE("Data Ready Wire Assertion","[uart-rx]"){
+    /*
+    The data ready wire should go high for one clock cycle after 9.5*CYCLES_PER_BIT clock cycles
+    */
+    auto* tb = new SyncTB<MODTYPE>(50000000, false); // make a new module test bench
+    REQUIRE(tb->dut->o_data_ready_w==0x00); //Data ready should initialize to zero
+    tb->dut->i_rx_w = 0x00;
+    int cyclesPerDataFrame = 9.5*TICKS_PER_CYCLE;
+    for(int j = 0; j<cyclesPerDataFrame;j++)
     {
         tb->tick();
     }
+    REQUIRE(tb->dut->uart_rx__DOT__system_state_r==7); //should be in the last clock cycle of b7 transmit
+    tb->tick();
+    REQUIRE(tb->dut->uart_rx__DOT__system_state_r==9); //should have now entered the latch data state
+    REQUIRE(tb->dut->o_data_ready_w == 0); // Data ready should not assert until the next cycle
+    tb->tick();
+    REQUIRE(tb->dut->o_data_ready_w == 1);
+}
+
+TEST_CASE("busy wire stays high through entire dataframe","[uart-rx]"){
+    /*
+    The busy wire should be combinatorial output and assert itself whenever the state is not idle 
+    */
+    auto* tb = new SyncTB<MODTYPE>(50000000, false); // make a new module test bench
+    REQUIRE(tb->dut->o_busy == 0x00); // By default we're not busy
+    //Kick off a data transfer
+    tb->dut->i_rx_w=0x00;
+    tb->tick();
+    bool busyFlagValid = true;
+
+    //iterate over 1000 clock cycles with the data ling toggling every cycle
+    for(int j=0;j<1000;j++)
+    {
+        tb->tick();
+        busyFlagValid &= !(tb->dut->o_busy && (tb->dut->uart_rx__DOT__system_state_r==0x0A));
+        busyFlagValid &= !(!tb->dut->o_busy && (tb->dut->uart_rx__DOT__system_state_r!=0x0A));
+        tb->dut->i_rx_w ^= 1; // XOR the input wire to toggle 
+    }
+    REQUIRE(busyFlagValid);
+}
+
+TEST_CASE("There's no counter issues for multiple sequential bytes","[uart-rx]"){
+    /*
+    If recieving multiple bytes in a row the counter should not lag and eventually cause incorrect state transitions
+    */
+    auto* tb = new SyncTB<MODTYPE>(50000000, false); // make a new module test bench
+    auto dataTransferWorking=true;
+    for(int j= 0;j<0x100;j++){
+        receiveByte(tb, j);
+        dataTransferWorking &= (tb->dut->o_data_w == j);
+    }
+    REQUIRE(dataTransferWorking);
+}
+
+TEST_CASE("Initial States are set properly","[uart-rx]"){
+    /*
+    All values should be initialized properly
+    */
+    auto* tb = new SyncTB<MODTYPE>(50000000, false); // make a new module test bench
+    REQUIRE(tb->dut->o_data_ready_w==0);
+    REQUIRE(tb->dut->o_data_w == 0x00);
+    REQUIRE(tb->dut->uart_rx__DOT__system_state_r == ST_IDLE);
+    REQUIRE(tb->dut->uart_rx__DOT__baud_counter_r == (int)(1.5*TICKS_PER_CYCLE-1));
 }
--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -9,9 +9,9 @@ module uart_rx#(
 (
     input wire i_clk, //System Clock
     input wire i_rx_w, // the data that will be latched and transmitted
-    output wire o_busy, //sitnal that's asserted when the state machine is not in idle
-    output reg o_data_ready_w, //signal that's asserted when new data has been latched to the data register
-    output reg[DATA_BITS-1:0] o_data_w
+    output wire o_busy, //signal that's asserted when the state machine is not in idle
+    output reg o_data_ready_w, //signal that's asserted when new data has been latched to the data register (only one for one clock cycle)
+    output reg[DATA_BITS-1:0] o_data_w //the most recently recieved complete data byte
 );
     reg[DATA_BITS-1:0] data_r; // Data that was recieved
     reg[DATA_BITS-1:0] rx_data_r; //Register that gets written to as data comes in
@@ -36,11 +36,12 @@ module uart_rx#(
     initial next_state_r = ST_IDLE;
 
     reg [7:0] baud_counter_r; //counter to trigger state machine 
-    initial baud_counter_r = CYCLES_PER_BIT[7:0]-1;
+    //initialize baud counter to the idle value
+    initial baud_counter_r =  CYCLES_PER_BIT[7:0]+(CYCLES_PER_BIT[7:0]>>1)-1;
 
     //State Machine to drive the UART transmitter
     always@(posedge i_clk) begin
-        o_data_ready_w<= 0; // By default data ready should be low
+        o_data_ready_w <= 0; // By default data ready should be low, and only high for one clock cycle
 
         //If the counter has zero'd out, increment to the next state
         //Because of how the counter is initialized at first, these transitions should happen
@@ -74,7 +75,7 @@ module uart_rx#(
     // Timing generation for state machine
     always@(posedge i_clk) begin
         if(system_state_r == ST_IDLE)
-            // The first time we cycle the counter we want it to be 0.5x the baud counter so that we trigger state transistions 
+            // The first time we cycle the counter we want it to be 1.5x the baud counter so that we trigger state transistions 
             // in the middle of the data bit
             baud_counter_r <= CYCLES_PER_BIT[7:0]+(CYCLES_PER_BIT[7:0]>>1)-1; //Don't increment counter when idling
         else begin
