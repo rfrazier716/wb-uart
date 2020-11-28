025534a39175a13fa63cdb90a78680ef1411f8e0
Ryan Frazier <rfrazier716@gmail.com>
2020-11-28 15:10:10 -0500

    working on example to transmit hello. Will send a hello message every second over uart

---
+++ rtl/hello_uart.v
@@ -0,0 +1,14 @@
+`default_nettype none //flags an error if you haven't defined a wire
+
+
+// A basic uart example that transmits a message every second
+
+module hello_uart(
+    input wire i_clk, // The system clock
+    output wire o_tx_w, // uart TX wire
+    output wire i_rx_w // uart Tx wire
+);
+
+
+
+endmodule;
